b2462632a0debeafe24cd4603b66c0a991b89153
Jei Jacinto <jjacinto@jei-jacintos-imac-2.local>
2011-05-22 15:06:31 -0700

    Initial Commit

---
+++ TestAaron1/TestAaron1AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  TestAaron1AppDelegate.h
+//  TestAaron1
+//
+//  Created by Jei Jacinto on 5/22/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class TestAaron1ViewController;
+
+@interface TestAaron1AppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet TestAaron1ViewController *viewController;
+
+@end
---
+++ TestAaron1/TestAaron1ViewController.h
@@ -0,0 +1,15 @@
+//
+//  TestAaron1ViewController.h
+//  TestAaron1
+//
+//  Created by Jei Jacinto on 5/22/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestAaron1ViewController : UIViewController {
+    
+}
+
+@end
